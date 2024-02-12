ce48d276adf3c1261197e9a3ee41e5cffbb1755e
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-12 13:21:54 -0800

    Added billboard to main page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Container from "@/components/reuseable/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
 
-const HomePage = () => {
-  return <div>Store Page</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("2b5c0bcc-d89a-4dec-ab60-b41071f17b35");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
