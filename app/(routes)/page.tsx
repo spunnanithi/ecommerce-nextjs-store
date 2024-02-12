877a7af84e74578cf98761aff1cafdae2546344c
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-12 14:01:05 -0800

    Created Products List and Products Card for main page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import Container from "@/components/reuseable/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("2b5c0bcc-d89a-4dec-ab60-b41071f17b35");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   );
---
+++ components/reuseable/icon-button.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { MouseEventHandler } from "react";
+
+interface IconButtonProps {
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  icon?: React.ReactElement;
+  className?: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  onClick,
+  icon,
+  className,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "flex items-center justify-center rounded-full border bg-white p-2 shadow-md transition hover:scale-110",
+        className,
+      )}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
