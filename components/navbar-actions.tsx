2a6851aeed759bd2562effc42694d4d6ff82628e
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-15 18:58:00 -0800

    Added add to cart functionality to cart button on product card

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/reuseable/button";
+import useCart from "@/hooks/useCart";
 import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
 
@@ -11,6 +12,8 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const cart = useCart();
+
   if (!isMount) {
     return null;
   }
@@ -19,7 +22,9 @@ const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
