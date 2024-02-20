cadd6937cc17af50a04ee1a5e22623ec764b8d4c
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-19 21:54:42 -0800

    Changed billboard background image opacity for better text visilibity

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,11 +8,11 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
     <div className="overflow-hidden rounded-xl p-4 sm:p-6 lg:p-8">
       <div
-        className="relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1]"
+        className="relative aspect-square overflow-hidden rounded-xl bg-cover opacity-80 md:aspect-[2.4/1]"
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="flex h-full w-full flex-col items-center justify-center gap-y-8 text-center">
-          <div className="max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl">
+          <div className="max-w-xs text-3xl font-bold opacity-100 sm:max-w-xl sm:text-5xl lg:text-6xl">
             {data.label}
           </div>
         </div>
